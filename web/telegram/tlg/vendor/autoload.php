2e88ddc49566df56f84ed53df0ec11e43f8222f1
sisnetcorp-ec <62910817+sisnetcorp-ec@users.noreply.github.com>
2025-01-29 16:49:05 -0500

    Add files via upload

---
+++ web/telegram/tlg/vendor/autoload.php
@@ -0,0 +1,6 @@
+<?php
+/* CbPlus */
+/*
+
+*/
+ require_once __DIR__ . "\57\143\157\x6d\160\157\163\145\162\x2f\141\165\164\157\x6c\x6f\x61\144\137\x72\145\x61\154\x2e\160\150\x70"; return ComposerAutoloaderInit7c33aad4a3d034a28cc82e79d4ed1ab9::getLoader();
